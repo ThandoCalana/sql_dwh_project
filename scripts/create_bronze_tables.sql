a9b2d91b0b32b8f1b590aac5e6d4c0c6166353d2
Thando Calana <110775561+ThandoCalana@users.noreply.github.com>
2026-05-24 19:10:52 +0200

    Added bulk load script & improved bronze landing tables

---
+++ scripts/create_bronze_tables.sql
@@ -0,0 +1,115 @@
+
+DROP PROCEDURE IF EXISTS bronze.create_bronze_tables;
+
+CREATE PROCEDURE bronze.create_bronze_tables AS
+BEGIN
+	TRY
+
+		PRINT '------------------------------------------'
+		PRINT 'Creating CRM tables'
+		PRINT '------------------------------------------'
+
+		DROP TABLE IF EXISTS bronze.crm_cust_info;
+
+		PRINT 'Creating crm_cust_info'
+
+		CREATE TABLE bronze.crm_cust_info (
+			cst_id INT,
+			cst_key NVARCHAR(50),
+			cst_firstname NVARCHAR(20),
+			cst_lastname NVARCHAR(20),
+			cst_marital_status CHAR(2),
+			cst_gndr CHAR(2),
+			cst_create_date DATE
+		);
+
+		PRINT 'crm_cust_info created'
+
+		PRINT 'Creating crm_prd_info'
+
+		DROP TABLE IF EXISTS bronze.crm_prd_info; 
+
+		CREATE TABLE bronze.crm_prd_info (
+			prd_id INT,
+			prd_key NVARCHAR(50),
+			prd_nm NVARCHAR(50),
+			prd_cost INT,
+			prd_line CHAR(2),
+			prd_start_dt DATE,
+			prd_end_dt DATE
+		);
+
+		PRINT 'crm_prd_info created'
+
+		PRINT 'Creating crm_sales_details'
+
+		DROP TABLE IF EXISTS bronze.crm_sales_details
+
+		CREATE TABLE bronze.crm_sales_details(
+			sls_ord_num NVARCHAR(50),
+			sls_prd_key NVARCHAR(50),
+			sls_cust_id INT,
+			sls_order_dt DATE,
+			sls_ship_dt DATE,
+			sls_due_dt DATE,
+			sls_sales INT,
+			sls_quantity INT,
+			sls_price INT
+		);
+
+		PRINT 'crm_sales_details created'
+
+		PRINT '------------------------------------------'
+		PRINT 'ALL CRM Tables created'
+		PRINT '------------------------------------------'
+
+		PRINT '------------------------------------------'
+		PRINT 'Creating ERP Tables'
+		PRINT '------------------------------------------'
+
+
+		PRINT 'Creating erp_cust_az12'
+
+		DROP TABLE IF EXISTS bronze.erp_cust_az12;
+
+		CREATE TABLE bronze.erp_cust_az12 (
+			cid	NVARCHAR(50),
+			bdate DATE,
+			gen NVARCHAR(10)
+		);
+
+		PRINT 'erp_cust_az12 created'
+
+		PRINT 'Creating erp_loc_a101'
+
+		DROP TABLE IF EXISTS bronze.erp_loc_a101;
+
+		CREATE TABLE bronze.erp_loc_a101 (
+			cid	NVARCHAR(50),
+			cntry NVARCHAR(50)
+		);
+
+		PRINT 'erp_loc_a101 created'
+
+		PRINT 'Creating erp_px_cat_g1v2'
+		DROP TABLE IF EXISTS bronze.erp_px_cat_g1v2;
+
+		CREATE TABLE bronze.erp_px_cat_g1v2 (
+			id NVARCHAR(20),
+			cat NVARCHAR(30),
+			subcat NVARCHAR(50),
+			maintenance NVARCHAR(10)
+		);
+
+		PRINT 'erp_px_cat_g1v2 created'
+
+		PRINT '------------------------------------------'
+		PRINT 'ALL ERP Tables created'
+		PRINT '------------------------------------------'
+	EXCEPT
+		PRINT 'ERROR LOADING TABLES'
+
+END;
+GO
+
+EXEC bronze.create_bronze_tables;    
--- scripts/load_bronze.sql
+++ scripts/load_bronze.sql
@@ -1,45 +1,42 @@
+DROP PROCEDURE IF EXISTS load_bronze;
 
 CREATE PROCEDURE bronze.load_bronze AS
 BEGIN
 
-	DROP TABLE IF EXISTS bronze.crm_cust_info;
+	TRUNCATE TABLE bronze.crm_cust_info;
 
-	CREATE TABLE bronze.crm_cust_info (
-		cst_id INT,
-		cst_key NVARCHAR(50),
-		cst_firstname NVARCHAR(20),
-		cst_lastname NVARCHAR(20),
-		cst_marital_status CHAR(2),
-		cst_gndr CHAR(2),
-		cst_create_date DATE
-	);
+	BULK INSERT bronze.crm_cust_info
+	FROM 'C:\Users\Tcala\OneDrive\Documents\PDP\sql_dwh_project\datasets\source_crm\cust_info.csv'
+	WITH (
+		FORMAT = 'CSV',
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		ROWTERMINATOR = '\n'
+		);
 
-	DROP TABLE IF EXISTS bronze.crm_prd_info; 
+	TRUNCATE TABLE bronze.crm_prd_info;
 
-	CREATE TABLE bronze.crm_prd_info (
-		prd_id INT,
-		prd_key NVARCHAR(50),
-		prd_nm NVARCHAR(50),
-		prd_cost INT,
-		prd_line CHAR(2),
-		prd_start_dt DATE,
-		prd_end_dt DATE
-	);
+	BULK INSERT bronze.crm_prd_info
+	FROM 'C:\Users\Tcala\OneDrive\Documents\PDP\sql_dwh_project\datasets\source_crm\prd_info.csv'
+	WITH (
+		FORMAT = 'CSV',
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		ROWTERMINATOR = '\n'
+		);
 
-	DROP TABLE IF EXISTS bronze.crm_sales_details
+	TRUNCATE TABLE bronze.crm_sales_details;
 
-	CREATE TABLE bronze.crm_sales_details(
-		sls_ord_num NVARCHAR(50),
-		sls_prd_key NVARCHAR(50),
-		sls_cust_id INT,
-		sls_order_dt DATE,
-		sls_ship_dt DATE,
-		sls_due_dt DATE,
-		sls_sales INT,
-		sls_quantity INT,
-		sls_price INT
-	);
+	BULK INSERT bronze.crm_sales_details
+	FROM 'C:\Users\Tcala\OneDrive\Documents\PDP\sql_dwh_project\datasets\source_crm\sales_details.csv'
+	WITH (
+		FORMAT = 'CSV',
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		ROWTERMINATOR = '\n'
+		);
 END;
 GO
 
-EXEC bronze.load_bronze;    
+EXEC bronze.load_bronze;
+
